63256c38994bc0153fdab9a4b5ed7b0e66bbffa5
k-zacky <38598480+k-zacky@users.noreply.github.com>
2018-05-24 14:52:49 +0000

    Update expection.c

--- src/Chapter4/expection.c
+++ src/Chapter4/expection.c
@@ -1,45 +1,23 @@
 ﻿#include<stdio.h>
-
 #include<stdlib.h>
-
 int main(){
-
   int a,b,c,i=0;
-
   double d=0.0;
-
   srand((unsigned)time(NULL));
-
   for(i=0;i<2000;i++){
-
     a=rand()%2;
-
     b=rand()%2;
-
     c=rand()%2;
-
     if(a==1){
-
       d++;
-
     }
-
     if(b==1){
-
       d++;
-
     }
-
     if(c==1){
-
       d++;
-
     }
-
-    printf("%d  %lf\n",i,d/i);
-
+    Printf("%d  %lf\n",i,d/i);
   }
-
 }
-
 /*コインを3枚投げた結果の相加平均。x軸試行回数、y軸結果*/
